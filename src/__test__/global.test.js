2e01bb1410f513d924aa9f7ffb4d7f6c963a7ef6
unknown <alejandrojzeballos@gmail.com>
2022-02-19 20:30:54 -0400

    {add} pruebas de tipos de datos

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,4 +1,28 @@
 const text = "hola mundo";
+const fruits = ['manzana', 'melon', 'banana'];
+
 test('debe tener un texto', ()=>{
     expect(text).toMatch(/mundo/); 
 })
+
+test('¿tenemos una banana?', ()=>{
+    expect(fruits).toContain('banana');
+});
+
+test('Mayor que', ()=>{
+    expect(10).toBeGreaterThan(9);
+});
+
+test('Verdadero', ()=>{
+    expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+};
+
+test('probar un callback', ()=>{
+    reverseString('hola', (str) => {
+        expect(str).toBe('aloh');
+    });
+});
