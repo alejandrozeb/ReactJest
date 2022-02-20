6dc4ace88cad8924c9ff6b24751d272d42b72d0c
unknown <alejandrojzeballos@gmail.com>
2022-02-19 20:46:49 -0400

    {add} pruebas en promesas y async await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,34 @@ test('probar un callback', ()=>{
         expect(str).toBe('aloh');
     });
 });
+
+const reverseString2 = str =>{
+    return new Promise((resolve, reject)=>{
+        if(!str){
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""));
+    });
+}
+
+test('Probar una promesa', () =>{
+    return reverseString2('Hola')
+    .then(string=>{
+        expect(string).toBe('aloH')
+    })
+});
+
+test('Probar async/await', async()=>{
+    const string = await reverseString2('hola');
+    expect(string).toBe('aloh');
+});
+
+
+//correr codigo antes y despues de cada prueba
+
+afterEach(()=> console.log('Despues de cada prueba'));
+afterAll(()=> console.log('Despues de todas las pruebas'));
+
+beforeEach(()=> console.log('antes de cada prueba'));
+beforeAll(()=> console.log('antes de todas las pruebas'));
+//para reiniciar mocks y stubs 
